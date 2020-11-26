7e93bcbbc1682163370be8a563c9402047c7ed61
Hugo Delile <hugo.delile@outlook.com>
2020-11-26 16:24:23 +0100

    to cuda, add layers, add coor evaluation

--- net.py
+++ net.py
@@ -10,27 +10,26 @@ from dataset_det import Balls_CF_Detection, COLORS
 
 batch_size = 100
 test_batch_size = 100
-epochs = 10
+epochs = 300
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3, 1)
-        self.conv2 = nn.Conv2d(64, 128, 3, 1)
+        self.conv1 = nn.Conv2d(3, 128, 3, 1)
+        self.conv2 = nn.Conv2d(128, 256, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(12800, 128)
-        self.fc2 = nn.Linear(128, 9)
-        self.fcbb = nn.Linear(128, 4*9)
+        self.fc1 = nn.Linear(135424, 512)#25600 135424
+        self.fc2 = nn.Linear(512, 64)
+        self.fc3 = nn.Linear(64, 9)
+        self.fcbb1 = nn.Linear(512, 4*9*5)
+        self.fcbb2 = nn.Linear(4*9*5, 4*9)
 
     def forward(self, x):
         x = self.conv1(x)
-        # print(x.size())
         x = F.relu(x)
-        # print(x.size())
         x = self.conv2(x)
-        # print(x.size())
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
@@ -39,9 +38,18 @@ class Net(nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout2(x)
-        # y = self.fcbb(x)
+
+        y = self.fcbb1(x)
         x = self.fc2(x)
-        return x# , y
+        x = F.relu(x)
+        # x = self.dropout2(x)
+        
+        y = F.relu(y)
+        # y = self.dropout2(y)
+
+        y = self.fcbb2(y)
+        x = self.fc3(x)
+        return x, y
 #weight=torch.tensor([1,2])
 
 def train(model, train_loader, optimizer, epoch):
@@ -49,21 +57,27 @@ def train(model, train_loader, optimizer, epoch):
     correct = 0
     for batch_idx, (data, target, bounding_box) in enumerate(train_loader):
         optimizer.zero_grad()
-        outcol = model(data)
+        data = data.to("cuda")
+        target = target.to("cuda")
+        bounding_box = bounding_box.to("cuda")
+        outcol, outcoor = model(data)
+        
         bce = torch.nn.BCEWithLogitsLoss() # try without weight
         # bce = torch.nn.BCEWithLogitsLoss(pos_weight=torch.add(torch.flatten(target), 1)) # try without weight
         losscol = bce(torch.flatten(outcol), torch.flatten(target).float())
-        # mse = torch.nn.MSELoss()
-        # losscoor = mse(torch.flatten(outcol), torch.flatten(target).float())
-        sumloss = losscol#  + mse
+        mse = torch.nn.MSELoss()
+        losscoor = mse(torch.flatten(outcoor), torch.flatten(bounding_box).float())
+        
+        sumloss = losscol + losscoor
         sumloss.backward()
         optimizer.step()
         pred = torch.sigmoid(outcol) > 0.5  # get the index of the max log-probability
         correct += pred.eq(target).sum().item()
         if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tColor loss: {:.6f}\tCoor. loss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), sumloss.item()))
+                100. * batch_idx / len(train_loader), losscol.item(), losscoor.item()))
+
     correct /= 9
     print('\nTrain set: Accuracy: {}/{} ({:.0f}%)\n'.format(correct, len(train_loader.dataset),
                 100. * correct / len(train_loader.dataset)))
@@ -71,27 +85,43 @@ def train(model, train_loader, optimizer, epoch):
 
 def test(model, test_loader):
     model.eval()
-    test_loss = 0
-    correct = 0
+    test_loss_col = 0
+    test_loss_coor = 0
+    correct_col = 0
+    correct_coor = 0
     with torch.no_grad():
         for data, target, bounding_box in test_loader:
-            output = model(data)
+            outcol, outcoor = model(data.to("cuda"))
+            target = target.to("cuda")
+            bounding_box = bounding_box.to("cuda")
             # crit = torch.nn.BCEWithLogitsLoss(pos_weight=torch.add(torch.flatten(target), 1))
-            crit = torch.nn.BCEWithLogitsLoss()
-            test_loss += crit(torch.flatten(output), torch.flatten(target).float()).item()  # sum up batch loss
-            pred = torch.sigmoid(output) > 0.5  # get the index of the max log-probability
-            correct += pred.eq(target).sum().item()
-    
-    correct /= 9
-    test_loss /= len(test_loader.dataset)
+            critcol = torch.nn.BCEWithLogitsLoss()
+            test_loss_col += critcol(torch.flatten(outcol), torch.flatten(target).float()).item()  # sum up batch loss
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+            crit_coor = torch.nn.BCEWithLogitsLoss()
+            test_loss_coor += critcol(torch.flatten(outcoor), torch.flatten(bounding_box).float()).item()
+            
+            outcoor = torch.reshape(outcoor, (100, 9, 4))
+
+            pred_coor = torch.abs(bounding_box - outcoor)
+            
+            vector_to_check = pred_coor[target == 1]
+            correct_coor += (vector_to_check.sum(dim=1)).sum().item()/(1200)
+            # print(correct_coor)
+            pred_col = torch.sigmoid(outcol) > 0.5  # get the index of the max log-probability
+            correct_col += pred_col.eq(target).sum().item()
+    
+    correct_col /= 9.0
+    correct_coor /= 9.0
+    test_loss_col /= len(test_loader.dataset)
+
+    print('\nTest set: Average color loss: {:.4f}, Average coor. loss: {:.4f}'.format(test_loss_col, correct_col))
+    print('Accuracy col: {}/{} ({:.0f}%)'.format(correct_col, len(test_loader.dataset), 100.0 * correct_col / len(test_loader.dataset)))
+    print('Accuracy coor: {}/{} ({:.0f}%)\n'.format(correct_coor, len(test_loader.dataset), 100.0 * correct_coor / len(test_loader.dataset)))
 
 
 def main():
-    torch.manual_seed(0)
+    torch.seed()
 
     dataset_train = Balls_CF_Detection("data\\train\\train\\")
     dataset_test = Balls_CF_Detection("data\\train\\val\\")
@@ -99,8 +129,8 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=test_batch_size)
 
-    model = Net()
-    optimizer = optim.Adadelta(model.parameters(), lr=0.001)
+    model = Net().to("cuda")
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
 
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1)
     for epoch in range(1, epochs):
